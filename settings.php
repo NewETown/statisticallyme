73c21061651b32104f626053ebe9f73d3c566abd
Everett Carney <everett@mentaltangent.net>
2014-02-26 12:52:16 -0800

    GA tracking update

--- settings.php
+++ settings.php
@@ -12,14 +12,6 @@ function getHeader() {
 	echo("<link rel=\"stylesheet\" type=\"text/css\" href=\"css/custom.css\">\n");
 	echo("<link href='http://fonts.googleapis.com/css?family=Istok+Web:400,400italic' rel='stylesheet' type='text/css'>\n");
 	echo("<script src=\"http://code.jquery.com/jquery-latest.min.js\"></script>\n");
-	echo("<script>");
-  	echo("\t(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){");
-  	echo("\t(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),");
-  	echo("\tm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)");
-  	echo("\t})(window,document,'script','//www.google-analytics.com/analytics.js','ga');");
-  	echo("\tga('create', 'UA-42403118-7', 'statistically.me');");
-  	echo("\tga('send', 'pageview');");
-	echo("</script>");
 	echo("</head>\n");
 	echo("<body>\n");
 	echo("\t<div id=\"fb-root\"></div>\n");
@@ -67,6 +59,15 @@ function getFooter() {
 	// echo("\t\t\t</div>");
 	echo("\t\t</div>");
 	echo("\t</footer>");
+
+	echo("\t<script>");
+  	echo("\t\t(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){");
+  	echo("\t\t(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),");
+  	echo("\t\tm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)");
+  	echo("\t\t})(window,document,'script','//www.google-analytics.com/analytics.js','ga');");
+  	echo("\t\tga('create', 'UA-42403118-7', 'statistically.me');");
+  	echo("\t\tga('send', 'pageview');");
+	echo("\t</script>");
 }
 
 ?>
