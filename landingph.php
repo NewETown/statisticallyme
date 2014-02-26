53e0d924882d9abf15b22c20b282c84109d884e9
Everett Carney <everett@mentaltangent.net>
2014-02-26 12:37:01 -0800

    Adding the landing PH page

---
+++ landingph.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<<html>
+<head>
+	<title>Statistically.Me</title>
+	<?php
+  echo("\t<link rel=\"stylesheet\" type=\"text/css\" href=\"css/bootstrap.css\">\n");
+  echo("\t<link rel=\"stylesheet\" type=\"text/css\" href=\"css/custom.css\">\n");
+  echo("\t<link href='http://fonts.googleapis.com/css?family=Istok+Web:400,400italic' rel='stylesheet' type='text/css'>\n");
+  echo("\t<script src=\"http://code.jquery.com/jquery-latest.min.js\"></script>\n");
+	echo("\t<script>");
+  	echo("\t\t(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){");
+  	echo("\t\t(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),");
+  	echo("\t\tm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)");
+  	echo("\t\t})(window,document,'script','//www.google-analytics.com/analytics.js','ga');");
+  	echo("\t\tga('create', 'UA-42403118-7', 'statistically.me');");
+  	echo("\t\tga('send', 'pageview');");
+	echo("\t</script>");
+	?>
+</head>
+<body>
+  <div class="text-center" style="margin-top:20%;">
+    <h5>Learn about yourself</h5>
+    <h1>Coming Soon</h1>
+  </div>
+</body>
+</html>
