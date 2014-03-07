45f3d07f009e66fb78ceb0b4d6a32790cd2ccd8f
Everett Carney <everett@mentaltangent.net>
2014-03-07 10:14:08 -0800

    Moving the connection inside the map query file

--- map_queries.php
+++ map_queries.php
@@ -30,7 +30,18 @@ ORDER BY m.fb_id, i.name;
 
 */
 
-function getMatchingFacebookLikes($conn) {
+ini_set('max_execution_time', 300); //300 seconds = 5 minutes
+
+require_once 'dbconfig.php';
+
+try {
+	$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+} catch (PDOException $pe){
+	die("Could not connect to the database $dbname: " . $pe->getMessage());
+}
+
+
+function getMatchingFacebookLikes() {
 
 	// PC::db("Checking like refresh date for ". $user_id);
 
@@ -48,13 +59,13 @@ function getMatchingFacebookLikes($conn) {
 	try {
 		$sql = 'SELECT DISTINCT (m.fb_id), u.lat, u.lng FROM interests AS i INNER JOIN  interest_map AS m ON ( i.id = m.interest_id ) INNER JOIN users AS u ON m.fb_id = u.fb_id WHERE i.category = :category AND EXISTS (SELECT * FROM interest_map AS m2 WHERE m2.fb_id = :fb_id AND m2.interest_id = m.interest_id)';
 
-		$q = $conn->prepare($sql);
 
 		$task = array(
 			':fb_id' => $fb_id,
 			':category' => $category
 			)
 
+		$q = $conn->prepare($sql);
 		$q->execute($task);
 		$q->setFetchMode(PDO::FETCH_ASSOC);
 
